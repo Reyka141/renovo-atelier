53b1cdc9b180dcab985615d22261fefc688a149c
Andrii Veduta <youdronveduta@gmail.com>
2025-11-25 14:19:36 +0100

    fix: adjust padding in BasketPage for improved layout consistency
    - Modified the padding values in the `BasketPage` component to enhance responsiveness across different screen sizes.
    - This change aims to improve the overall user experience by ensuring better alignment and spacing in the layout.

--- src/app/[locale]/basket/page.tsx
+++ src/app/[locale]/basket/page.tsx
@@ -4,7 +4,7 @@ export default async function BasketPage() {
     return (
         <div className="bg-white">
             <Header textColor="black" />
-            <main className="flex flex-col gap-9 pt-[136px] xl:gap-16 2xl:gap-28">
+            <main className="flex flex-col gap-9 pt-[102px] md:pt-[120px] lg:pt-[136px] xl:gap-16 2xl:gap-28">
                 <Basket />
                 <Footer />
             </main>
